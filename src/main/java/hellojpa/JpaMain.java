4cf8c98759af98fc5d7af831254ddac7bc64202d
noname_dev <noname_dev@naver.com>
2022-10-25 21:43:15 +0900

    Hello JPA - 애플리케이션 개발

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //회원 등록
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
+//            em.persist(member);
+            //회원 조회
+//            Member findMember = em.find(Member.class, 1L);
+            //회원 삭제
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+            //회원 수정
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
